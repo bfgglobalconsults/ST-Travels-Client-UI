a26268c4c8f515afc2b060f05bcde1f112980db2
David Bradford <davidbradforduko@yahoo.com>
2024-05-31 14:10:35 +0100

    api folders

--- src/app/api/flight/route.ts
+++
@@ -1,8 +0,0 @@
-import jsonData from "./db.json";
-
-export async function GET(request: Request) {
-  const data = JSON.stringify(jsonData);
-  const headers = { "Content-Type": "application/json" };
-
-  return new Response(data, { headers });
-}
