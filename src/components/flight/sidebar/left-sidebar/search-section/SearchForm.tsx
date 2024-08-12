348ae339442a5eb150d96bfd8c01cc0d9a8a0e67
David Bradford <davidbradforduko@yahoo.com>
2024-08-12 11:38:43 +0100

    search form issue fixed

--- src/components/flight/sidebar/left-sidebar/search-section/SearchForm.tsx
+++ src/components/flight/sidebar/left-sidebar/search-section/SearchForm.tsx
@@ -1,3 +1,4 @@
+"use client"
 import FlightClass from "@/components/common/booking-form/flight-form/flight-class";
 import QtyBox from "@/components/common/booking-form/flight-form/qty-box";
 import Button from "@/components/common/btn";
@@ -14,63 +15,103 @@ interface  SearchForm {
 const SearchForm = ({toggleForm}:SearchForm) => {
     const [startDate, setStartDate] = useState(new Date());
     const [startDate1, setStartDate1] = useState(new Date());
+        const [flightClass, setFlightClassState] = useState("Economy");
+
   
   return (
-    <form onSubmit={(event: React.FormEvent<HTMLFormElement>)=>event.preventDefault()} className="row m-0 flight-form-input">
-              <div className="col-lg-2">
-                <div className="form-group">
-                  <label>{From}</label>
-                  <input type="text" className="form-control open-select" id="exampleInputEmail1" defaultValue="dubai(DXB)" placeholder="from" />
-                  <Img src="/assets/images/icon/from.png" className="img-fluid" alt="" />
-                </div>
-              </div>
-              <div className="col-lg-2">
-                <div className="form-group">
-                  <label>{To}</label>
-                  <input type="text" className="form-control open-select" defaultValue="paris(PAR)" placeholder="to" />
-                  <Img src="/assets/images/icon/location.png" className="img-fluid" alt="" />
-                </div>
-              </div>
-              <div className="col-lg-2">
-                <div className="form-group">
-                  <label>{Departure}</label>
-                  <div className="form-control">
-                  <DatePickerComponent start={startDate} setStart={setStartDate} />
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-2">
-                <div className="form-group">
-                  <label>{Return}</label>
-                  <div className="form-control">
-                  <DatePickerComponent start={startDate1} setStart={setStartDate1} />
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-2">
-                <div className="form-group">
-                  <label>{TravellerClass}</label>
-                  <input type="text" className="form-control  open-select" defaultValue="1 traveller" placeholder="to" />
-                  <Img src="/assets/images/icon/user.png" className="img-fluid" alt="" />
-                  <div className="selector-box-flight">
-                    <QtyBox />
-                    <FlightClass />
-                    <div className="bottom-part">
-                      <Button btnClass="btn btn-solid color1" name={Apply} />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-2">
-                <div className="search-btn">
-                  <Link href="/flight/listing/left-sidebar"><Button btnClass="btn btn-solid color1" name={Search} /></Link>
-                </div>
-              </div>
-              <div className="responsive-close" onClick={toggleForm}>
-                <i className="far fa-times-circle"></i>
-              </div>
-            </form>
-  )
+    <form
+      onSubmit={(event: React.FormEvent<HTMLFormElement>) =>
+        event.preventDefault()
+      }
+      className="row m-0 flight-form-input"
+    >
+      <div className="col-lg-2">
+        <div className="form-group">
+          <label>{From}</label>
+          <input
+            type="text"
+            className="form-control open-select"
+            id="exampleInputEmail1"
+            defaultValue="dubai(DXB)"
+            placeholder="from"
+          />
+          <Img
+            src="/assets/images/icon/from.png"
+            className="img-fluid"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="col-lg-2">
+        <div className="form-group">
+          <label>{To}</label>
+          <input
+            type="text"
+            className="form-control open-select"
+            defaultValue="paris(PAR)"
+            placeholder="to"
+          />
+          <Img
+            src="/assets/images/icon/location.png"
+            className="img-fluid"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="col-lg-2">
+        <div className="form-group">
+          <label>{Departure}</label>
+          <div className="form-control">
+            <DatePickerComponent start={startDate} setStart={setStartDate} />
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-2">
+        <div className="form-group">
+          <label>{Return}</label>
+          <div className="form-control">
+            <DatePickerComponent start={startDate1} setStart={setStartDate1} />
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-2">
+        <div className="form-group">
+          <label>{TravellerClass}</label>
+          <input
+            type="text"
+            className="form-control  open-select"
+            defaultValue="1 traveller"
+            placeholder="to"
+          />
+          <Img
+            src="/assets/images/icon/user.png"
+            className="img-fluid"
+            alt=""
+          />
+          <div className="selector-box-flight">
+            <QtyBox />
+            <FlightClass
+              flightClass={flightClass}
+              setFlightClassState={setFlightClassState}
+            />
+            <div className="bottom-part">
+              <Button btnClass="btn btn-solid color1" name={Apply} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-2">
+        <div className="search-btn">
+          <Link href="/flight/listing/left-sidebar">
+            <Button btnClass="btn btn-solid color1" name={Search} />
+          </Link>
+        </div>
+      </div>
+      <div className="responsive-close" onClick={toggleForm}>
+        <i className="far fa-times-circle"></i>
+      </div>
+    </form>
+  );
 }
 
 export default SearchForm
